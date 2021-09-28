4eb71fd3978feccea553aaaff30063da8da4a3e2
tranhuyhoangbka <hoangth92.nd@gmail.com>
2021-09-28 12:35:58 +0700

    Projects can now be deleted

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -35,6 +35,12 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path, notice: 'Project has been deleted.'
+  end
+
   private
 
   def project_params
