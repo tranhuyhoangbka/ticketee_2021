293a2f7353d0a2325f1569f2cd01ef39b4b7facf
tranhuyhoangbka <hoangth92.nd@gmail.com>
2021-09-28 10:59:21 +0700

    Add title functionality for project show page

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -9,5 +9,9 @@ RSpec.feature "User can create new project" do
     click_button 'Create Project'
 
     expect(page).to have_content 'Project has been created.'
+    project = Project.find_by!(name: 'Visual Studio Code')
+    expect(page.current_url).to eq project_url(project)
+    title = 'Visual Studio Code - Projects - Ticketee'
+    expect(page).to have_title title
   end
 end
