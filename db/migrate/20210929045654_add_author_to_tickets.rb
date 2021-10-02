7abb5b044e73dd9b873b007b69a867e4dc8a4f8b
tranhuyhoangbka <hoangth92.nd@gmail.com>
2021-10-02 12:52:45 +0700

    Fix error migration AddAuthorToTickets

--- db/migrate/20210929045654_add_author_to_tickets.rb
+++ db/migrate/20210929045654_add_author_to_tickets.rb
@@ -1,6 +1,6 @@
 class AddAuthorToTickets < ActiveRecord::Migration[6.1]
   def change
     add_reference :tickets, :author, foreign_key: {to_table: :users}
-    change_column_null :tickets, :author_id, false, User.first.id
+    change_column_null :tickets, :author_id, false
   end
 end
